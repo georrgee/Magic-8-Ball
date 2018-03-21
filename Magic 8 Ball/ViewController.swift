ed7dcd8d646a1701b5581fa0dfd292af0c0c20e7
George Garcia <geor@vicki-2.local>
2018-03-20 21:04:46 -0700

    We can use a string instead of the array for the image to change. However, they both end with the same results.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //let answers = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randoNum = 0
+    
+    @IBOutlet weak var eightImage: UIImageView!
+    @IBOutlet weak var askButton: UIButton!
+    
+    @IBAction func askTouch(_ sender: AnyObject) {
+        
+        newBallImage()
+        
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        newBallImage()
+    }
+    
+    func newBallImage(){
+        
+        randoNum = Int(arc4random_uniform(5))
+        //eightImage.image = UIImage(named: answers[randoNum])
+        eightImage.image = UIImage(named: "ball\(randoNum + 1)")
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newBallImage()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
